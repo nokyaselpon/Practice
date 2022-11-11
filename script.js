513adf9c95e64983cfa98b1babae5d7f039b7502
nokyaselpon <75005487+nokyaselpon@users.noreply.github.com>
2022-11-11 22:12:28 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const hamburger = document.querySelector(".hamburger");
 const navmenu = document.querySelector(".navmenu");
 const hamburgerr = document.getElementById("hamburger");
 const navmenuu = document.getElementById("navmenu");
+const showcase = document.querySelector(".showcase");
+const showcasee = document.getElementById("showcase");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navmenu.classList.toggle("active");
+    showcase.classList.toggle("active");
 })
 
 document.onclick = function(clicks){
-    if(clicks.target.id !== "hamburger" && clicks.target.id !== "navmenu")
+    if(clicks.target.id !== "hamburger" && clicks.target.id !== "navmenu" && clicks.target.id !== "showcase")
     {
         hamburger.classList.remove("active");
         navmenu.classList.remove("active");
+        showcase.classList.remove("active");
     }
 }
+
